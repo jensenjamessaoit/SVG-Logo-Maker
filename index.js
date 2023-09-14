0b4125eca8e95443c05176aff49f3bd50760f439
jj <saoitjensen@gmail.com>
2023-09-13 20:35:41 -0600

    writetofile working and now makes logos

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 // npm and node packages
 const inquirer = require('inquirer');
 const fs = require('fs');
-
-// Objects
+const { Square, Circle, Triangle } = require('./lib/shapes');
 
 const inqPrompts = [
     {
@@ -28,8 +27,8 @@ const inqPrompts = [
     },
 ];
 
-const writeLogo = (data) => {
-    fs.writeFile('logo.svg', data, (err) =>{
+const writeToFile = (data) => {
+    fs.writeFile('./examples/logo.svg', data, (err) =>{
         if (err) {
             console.log(err);
         }
@@ -41,15 +40,23 @@ const writeLogo = (data) => {
 
 const init = () => {
     inquirer
-        .prompt([
-            inqPrompts[0],
-            inqPrompts[1],
-            inqPrompts[2],
-            inqPrompts[3],
-        ])
-        .then((res)=>{
-            console.log(res);
+        .prompt(inqPrompts)
+        .then((res) => {
+            switch (res.shape) {
+                case 'square':
+                    const square =  new Square(res.text, res.textColor, res.shapeColor);
+                    writeToFile(square.create());
+                    break;
+                case 'circle':
+                    const circle = new Circle(res.text, res.textColor, res.shapeColor);
+                    writeToFile(circle.create());
+                    break;
+                case 'triangle':
+                    const triangle = new Triangle(res.text, res.textColor, res.shapeColor);
+                    writeToFile(triangle.create());
+                    break;
+            };
         });
-}
+};
 
 init();
